c6efc7e9eeffeea4ae8693bb7c6ae4e48e872843
knowsyash <codeknowsyash@gmail.com>
2024-10-28 23:55:42 +0530

    access to dark mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
+
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black'; // Use 'black' for dark mode
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white'; // Use 'white' for light mode
+    }
+  };
+
   return (
     <>
-    <Navbar/>
+      <Navbar mode={mode} toggleMode={toggleMode} />
     </>
   );
 }
